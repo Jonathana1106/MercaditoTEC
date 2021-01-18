40cbc171407636a8468c4c480d3d17be02f61c5f
Jonathan G Araya <jonathana1196@gmail.com>
2021-01-17 18:20:59 -0600

    Coloreando
    Aun no logro subrayar el texto

--- MercaditoTECMobile/app/src/main/java/com/teammsn/mercaditotecmobile/LogIn.java
+++ MercaditoTECMobile/app/src/main/java/com/teammsn/mercaditotecmobile/LogIn.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.text.SpannableString ;
+import android.text.style.UnderlineSpan ;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
 
@@ -14,12 +17,17 @@ public class LogIn extends AppCompatActivity {
      * Se crean las variables globales de la clase
      */
     EditText email, password;
-    Button signin, signup;
+    TextView signup;
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+/**
+        SpannableString content = new SpannableString( "Content" ) ;
+        content.setSpan( new UnderlineSpan() , 0 , content.length() , 0 ) ;
+        signup.setText(content) ; */
 
         /**
          * Se inicializan las variables globales de la clase y se ligan al ID de la vista
@@ -27,7 +35,7 @@ public class LogIn extends AppCompatActivity {
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         signin = (Button) findViewById(R.id.btnsignin);
-        signup = (Button) findViewById(R.id.btnsignup);
+        signup = (TextView) findViewById(R.id.btnsignup);
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
