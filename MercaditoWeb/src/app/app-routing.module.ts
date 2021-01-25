41fc79d9c9e9a8dc487cb4509cd6090fbe35b105
Pablo <lpablouc9@gmail.com>
2021-01-25 13:13:26 -0600

    Arreglo de merge(faltaba una coma en el path de home en routing.module)

--- MercaditoWeb/src/app/app-routing.module.ts
+++ MercaditoWeb/src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent} from 'src/app/Components/Authentication/sign-up/sign-up.component';
 import { SignInComponent} from 'src/app/Components/Authentication/sign-in/sign-in.component';
 import { HomeComponent } from 'src/app/Components/home/home.component';
-import { OfertasComponent } from './Components/OfertasLaborales/ofertas.component';
-import { OfertaComponent } from './Components/Oferta/oferta.component';
-import { DashboardComponent } from './Components/Dashboard/dashboard.component';
+import { OfertasComponent } from './Components/OfertasLaborales/ofertas.component';
+
+import { OfertaComponent } from './Components/Oferta/oferta.component';
+
+import { DashboardComponent } from './Components/Dashboard/dashboard.component';
+
 import { StudentPComponent } from 'src/app/Components/StudentComponents/profile/student-p/student-p.component';
 import { AdminPComponent } from './Components/AdminComponents/profile/admin-p/admin-p.component';
 import { EmployerPComponent } from './Components/EmployerComponents/profile/employer-p/employer-p.component';
@@ -16,7 +19,7 @@ const routes: Routes = [
   {path:'signIn', component:SignInComponent},
   { path: 'ofertas', component: OfertasComponent },
   { path: 'oferta', component: OfertaComponent },
-  {path:'dashboard', component: DashboardComponent}
+  {path:'dashboard', component: DashboardComponent},
   {path:'home', component:HomeComponent},
   {path:'sProfile', component:StudentPComponent},
   {path:'aProfile', component:AdminPComponent},
