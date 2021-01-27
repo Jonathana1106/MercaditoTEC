a4eefd8aec4e8ea63e9df42a1f92e05b63da4c2b
Josue Araya García <jdag98228@gmail.com>
2021-01-27 17:52:55 -0600

    más avances
    casi que todo listo para unir con api

--- MercaditoWeb/src/app/Components/AgregarOferta/agregaroferta.component.ts
+++ MercaditoWeb/src/app/Components/AgregarOferta/agregaroferta.component.ts
@@ -3,6 +3,9 @@ import { FirebaseService } from 'src/app/services/Auth/firebase.service';
 import { AdminFirebaseService } from 'src/app/services/auth/admin/admin-firebase.service';
 import { CurrentUserService } from 'src/app/services/auth/currentUser/current-user.service';
 
+import { of } from 'rxjs';
+declare var $: any;
+
 
 @Component({
   selector: 'app-AgregarOferta',
@@ -14,6 +17,21 @@ export class AgregarOfertaComponent implements OnInit {
   
 
   ngOnInit(): void {
+
+    $(document).ready(function () {
+      $("#enviar").click(function () {
+        var descripcion = $("#escribirDescripcion").val();
+        alert(descripcion);
+        var base = $("#base").val();
+        var requisitos = $("#requisitos").val();
+        var descripcionr = $("#descripcionr").val();
+        var nombre = $("#nombre").val();
+        var oportunidad = $("#oportunidad").val();
+        var empresa = $("#empresa").val();
+        var ubicacion = $("#ubicacion").val();
+        /*this.PatientsService.sendData(name,lastName,id,age,nationality,region,pathologies,states,medication,intern,uci);*/
+      });
+    })
 
   }
 
